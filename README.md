f506f17bedea91b8741a61a282f1b5c260b9009c
wissem <wissem.yahiaouii@gmail.com>
2022-10-16 03:20:15 +0200

    adjuust

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Description
+Random Global and SDDM Theme at bootup or periodically 
+
+# Dependencies 
+- no dependencies, run only in plasma kde 
+
+# Install 
+sh install.sh
+
+and 
+
+sh unisntall 
+
+to remove it :)
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
 
-
 echo $(kdialog --password "sudo password required") | sudo -S bash -c "rm /usr/local/bin/custom_theme.sh /etc/systemd/system/custom_theme.service" << EOF
 rm "$HOME/bin/global_theme" "$HOME/.config/autostart/custom_theme.desktop"
 EOF
